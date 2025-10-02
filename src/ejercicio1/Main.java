81b9a3f477d082672eacf57f6b8fb0e157d69855
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2025-10-02 11:12:55 +0200

    Annotations

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         OnsiteWorker onsiteWorker = new OnsiteWorker("Joe", "Harris", 24.50, 275.80);
         RemoteWorker remoteWorker = new RemoteWorker("Ana", "Johnassen", 32.45);
-        double salary = onsiteWorker.calculateSalary(149);
+        double salary = onsiteWorker.calculateSalary(135);
         double salary2 = remoteWorker.calculateSalary(149);
         System.out.println(onsiteWorker + "\n The salary is: " + salary + " Euros.");
         System.out.println(remoteWorker + "\n The salary is: " + salary2 + " Euros.");
